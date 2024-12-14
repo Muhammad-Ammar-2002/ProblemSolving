f479d39ce08868eeefe704c59dfa895912216ece
Muhammad Ammar <muhammad4ammar23@gmail.com>
2024-12-14 09:39:42 +0200

    Task 1 : printing three words in three lines

---
+++ CoreJavaProgrammingChallenges/Challange_1.java
@@ -0,0 +1,23 @@
+/*
+ *
+ * "Task 1 : printing three words in three lines"
+ * 
+ * use one System.out.println("");
+ * to print:
+ *  
+ * Hi 
+ * Hello 
+ * Bye
+ * 
+ * as it is.
+ */
+
+
+public class Challange_1 {
+    
+    public static void print(){
+
+        System.out.println("Hi \nHello \nBye");
+    }
+    
+}
---
+++ CoreJavaProgrammingChallenges/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+   
+
+        public static void main(String args[])
+        {
+    
+           Challange_1.print();
+    
+        }
+    
+    
+}
